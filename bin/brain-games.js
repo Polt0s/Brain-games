c0c95900339d77f863bec0a717022acff53ef357
poltos <poltos.pro@gmail.com>
2020-06-26 23:35:25 +0300

    linter error correction2

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 import userName from '../src/cli.js';
+
 console.log(`${'Hello,'} ${userName}${'!'}`);
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 console.log('Welcome to the Brain Games!');
 
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-Sync';
 
 const userName = readlineSync.question('May I have your name? ');
 export default userName;
