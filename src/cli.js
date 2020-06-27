7ddd6b714ac271211c4a2fcd73b34d615bb00de8
poltos <poltos.pro@gmail.com>
2020-06-27 22:56:54 +0300

    linter error correction4

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
-console.log('Welcome to the Brain Games!');
-
 import readlineSync from 'readline-Sync';
 
+console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 export default userName;
