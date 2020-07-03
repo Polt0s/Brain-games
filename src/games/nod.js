ac9a4a010ed371a5da451e012cef013c89c32045
poltos <poltos.pro@gmail.com>
2020-07-03 23:48:34 +0300

    linter error correction

--- src/games/nod.js
+++ src/games/nod.js
@@ -5,13 +5,13 @@ import getRandomNumber from '../getRandomNumber.js';
 const getLargestCommonFactor = (num1, num2) => {
   while (num1 !== num2) {
     if (num1 > num2) {
-      num1 = num1 - num2;
+      num1 -= num2;
     } else {
-      num2 = num2 - num1;
+      num2 -= num1;
     }
   }
   return num1;
-}
+};
 getLargestCommonFactor();
 
 const runGame = () => {
@@ -39,7 +39,7 @@ const runGame = () => {
         counter = 0;
       }
     }
-  }
+  };
   getRandomCommonFactor();
-}
+};
 export default runGame;
