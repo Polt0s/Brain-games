d0b7c8fbb848ea2fe134496d54da612ff7b6d3db
poltos <poltos.pro@gmail.com>
2020-07-21 13:17:24 +0300

    fix

--- src/games/even.js
+++ src/games/even.js
@@ -5,11 +5,11 @@ const REGULATIONS = 'Answer "yes" if the number is even, otherwise answer "no".'
 const MIN_VALUE = 1;
 const MAX_VALUE = 30;
 
-const isEven = (number) => number % 2 === 0 ? 'yes' : 'no';
+const isEven = (number) => number % 2 === 0;
 
 const getGameData = () => {
   const question = getRandomNumber(MIN_VALUE, MAX_VALUE);
-  const answer = isEven(question);
+  const answer = ((isEven(question)) ? 'yes' : 'no');
   return { question, answer };
 };
 
